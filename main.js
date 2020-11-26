002fac95dcdb4c5caac92a5bcb84029334cc33e2
Luis <helloworld512@outlook.com>
2020-11-26 15:33:31 +0800

    adjust main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-function getImages(){
-    return [
-        'https://images.pexels.com/photos/5702980/pexels-photo-5702980.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        'https://images.pexels.com/photos/4834891/pexels-photo-4834891.jpeg?cs=srgb&dl=pexels-oleg-zaicev-4834891.jpg&fm=jpg',
-        'https://images.pexels.com/photos/3521963/pexels-photo-3521963.jpeg?cs=srgb&dl=pexels-nadi-lindsay-3521963.jpg&fm=jpg'
-    ]
-}
+window.PuzzleImageList = [
+    'https://images.pexels.com/photos/5702980/pexels-photo-5702980.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    'https://images.pexels.com/photos/4834891/pexels-photo-4834891.jpeg?cs=srgb&dl=pexels-oleg-zaicev-4834891.jpg&fm=jpg',
+    'https://images.pexels.com/photos/3521963/pexels-photo-3521963.jpeg?cs=srgb&dl=pexels-nadi-lindsay-3521963.jpg&fm=jpg'
+]
