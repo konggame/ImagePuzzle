49532efd7c5f486edf561f42439aa662d68a8c59
Luis <helloworld512@outlook.com>
2020-12-03 15:13:05 +0800

    remove firework

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 window.RemotePuzzleImageList = {
     christmas: [
-        'https://i.postimg.cc/5NQbt6hd/pexels-photo-3303619.png',
-        'https://i.postimg.cc/j2HVz7tL/pexels-photo-3388335.jpg'
     ],
-    firework: [
-        'https://i.postimg.cc/qMmrQ223/pexels-photo-230895.jpg',
-        'https://i.postimg.cc/kMvq604w/pexels-photo-3037679.jpg',
-        'https://i.postimg.cc/qMB0816r/pexels-photo-3823271.jpg',
-        'https://i.postimg.cc/dVLYfD9S/pexels-photo-5344847.jpg'
-    ]
+    /*firework: [
+    ]*/
 }
 window.postMessage('RemotePuzzleImageLoaded');
